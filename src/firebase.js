d449f3ad864198d47ddb6534263992bc14fe782c
CPLTOURNAMENT <hondashine5479@gmail.com>
2025-06-22 00:02:48 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
@@ -16,3 +17,23 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(); // ✅ THIS LINE
+=======
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "coc-tournament-5479",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(); // ✅ THIS LINE
+>>>>>>> 749f9e4ff8f7d61bf9e77bcf2ea3ea4a2f8fe604
